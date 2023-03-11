3c31081a7ff941e54924c0f8b305cf453268a544
Dionys Rosario <dionyself@gmail.com>
2023-03-10 22:41:39 -0400

    Generic fixes

--- studyhelper/start.sh
+++ studyhelper/start.sh
@@ -12,6 +12,6 @@ python manage.py migrate
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.get(username='admin', is_superuser=True).delete()"
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('$STUDY_HELPER_ADMIN_USERNAME', '$STUDY_HELPER_ADMIN_EMAIL', '$STUDY_HELPER_ADMIN_PASSWORD')"
 if [ $STUDY_HELPER_INITIAL_DATA -eq "1" ]; then
-    python manage.py shell -c "from quiz.views import import_json_course; import_json_course(open('/root/StudyHelper/studyhelper/data/initial.json', 'r'))"
+    python manage.py shell -c "from quiz.views import import_json_course; import_json_course([open('/root/StudyHelper/studyhelper/data/initial.json', 'r')])"
 fi
 python manage.py runserver 0.0.0.0:8000
