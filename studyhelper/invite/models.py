bc6ccbd37028ce10a043d0bdf2b097a92bfe4599
Dionys Rosario <dionyself@gmail.com>
2023-04-11 03:01:37 -0400

    Ordering questions by expertise level

--- studyhelper/invite/models.py
+++ studyhelper/invite/models.py
@@ -61,6 +61,23 @@ class AnonymousCourseSession(TimeStampedModel):
             session_score.is_enabled = False
             session_score.save()
             return
+
+        if not self.enforce_expertise_level:
+            expertise_gear=[]
+            for exp_filter in expertise_filter["expertise_level__in"]:
+                expertise_gear.append(remaining_questions.filter(expertise_level=exp_filter))
+
+            if self.max_n_questions:
+                position_val = self.max_n_questions // len(expertise_filter["expertise_level__in"])
+                position_index = len(used_questions_pk) // position_val
+                if expertise_gear[position_index].exists():
+                    return random.choice(expertise_gear[position_index])
+            else:
+                position_val = (self.closes_at - self.opens_at) // len(expertise_filter["expertise_level__in"])
+                position_index = (datetime.now(timezone.utc) - self.opens_at) // position_val
+                if expertise_gear[position_index].exists():
+                    return random.choice(expertise_gear[position_index])
+
         return random.choice(remaining_questions)
     
     def evaluate_attempt(self, attempted_question, selected_choices, quiz_profile):
